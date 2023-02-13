ae37a082a1c8ceb53e92257a8760d3197cc417bd
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-13 11:07:42 -0300

    🚧
    Add novo perfil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Seo } from '@/components/common/Seo'
 export default function Home() {
   return (
     <>
-      <Seo title='hello' />
+      <Seo title='Home' />
       <div className='h-screen pl-16 flex justify-center items-center bg-stone-800'>
         <h1 className='text-5xl text-white'>Home</h1>
       </div>
